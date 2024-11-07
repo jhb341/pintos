6128bc50bc02fc8f1caba537ac58965420ed403d
jhb341 <jhb341@postech.ac.kr>
2024-11-07 17:04:22 +0900

    아직 pagedir 오류 안고쳐짐

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,12 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+
 void syscall_init (void);
+
+bool verify_thread_memory(void *addr);
+
+static void syscall_handler (struct intr_frame *f);
 
 #endif /* userprog/syscall.h */
